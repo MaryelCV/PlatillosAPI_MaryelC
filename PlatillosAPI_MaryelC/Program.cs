d834b851630af11857be2041386bc12097b83211
MaryelCV <109971802+MaryelCV@users.noreply.github.com>
2024-03-07 19:31:23 -0600

    Add project files.

---
+++ PlatillosAPI_MaryelC/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using PlatillosAPI_MaryelC.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+
+        //Acá agregamos los valores de conexión a la base de da6tos para nuestra applicación 
+
+        //1. agregar la info en una una etiqueta en el archivo appsettins.json 
+
+        var CnnStrBuilder = new SqlConnectionStringBuilder(
+            builder.Configuration.GetConnectionString("CNNSTR")
+            );
+
+        //por seguridad es mejor agregar la info de la contraseña en este código o 
+        //podrímos tener un almacen externo. 
+        //Investigar qué son los User Secrets
+        CnnStrBuilder.Password = "progra6";
+
+        //ahora que hemos extraido la info de la cadena de conexión 
+        //creamos una variable tipo string que la almacene 
+        string CnnStr = CnnStrBuilder.ConnectionString;
+
+        //ahora conectamos el proyecto a la base de datos 
+        builder.Services.AddDbContext<PlatillosDbContext>(options => options.UseSqlServer(CnnStr));
+
+
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
